fc6dc86a38bbf5825d3326efe816b56a2bbea7df
Anuj Tailwal <anujtailwal72@gmail.com>
2025-11-17 11:43:34 +0530

    Implemented add method to handle custom delimiters

--- app/models/string_calculator.rb
+++ app/models/string_calculator.rb
@@ -3,6 +3,12 @@ class StringCalculator < ApplicationRecord
   def self.add(numbers)
     return 0 if numbers.empty?
 
-    numbers.gsub("\n", ",").split(',').map(&:to_i).sum
+    if numbers.start_with?("//")
+      delimiter = numbers[2]
+      numbers = numbers.split("\n", 2).last
+      numbers.split(delimiter).map(&:to_i).sum
+    else
+      numbers.gsub("\n", ",").split(',').map(&:to_i).sum
+    end
   end
 end
